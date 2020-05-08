9d8c6824ba52443731536a0d8e04f96935e5228e
Vladimir Priemyshev <leadmer35@gmail.com>
2020-05-08 18:44:02 +0300

    heroku

--- src/main/kotlin/etu/vt/trpo_backend/trpo_backend/Controllers/MainController.kt
+++ src/main/kotlin/etu/vt/trpo_backend/trpo_backend/Controllers/MainController.kt
@@ -45,10 +45,10 @@ class MainController {
      *   @param {ByteArray} ByteArray picture witch get from User
      *   @return {ResponsePictureData} DataClass for sending result on request
      **/
-    @RequestMapping(value = ["/picture"],
-            headers = ["Content-Type= multipart/form-data"],
-            method = [RequestMethod.POST],
-            consumes = [MediaType.MULTIPART_MIXED_VALUE])
+    @RequestMapping(
+            value = ["/picture"],
+            method = [RequestMethod.POST])
+//            consumes = [MediaType.MULTIPART_MIXED_VALUE])
     @ResponseBody
     fun pictureResponse(@RequestParam("imageRequest") request: MultipartFile): ResponsePictureData{
         val success = "Success"
